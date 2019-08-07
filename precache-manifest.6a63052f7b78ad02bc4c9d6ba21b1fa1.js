ebebddb796a737caf80b2904843a5ef11857c26b
Lucas Sena <lucassena.rj@gmail.com>
2019-08-07 15:34:55 +0100

    Updates

---
+++ precache-manifest.6a63052f7b78ad02bc4c9d6ba21b1fa1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "350dcfb199a573e6cd98cd9dc9ff6ae8",
+    "url": "/my-favorite-movies/index.html"
+  },
+  {
+    "revision": "0128f80739380961f351",
+    "url": "/my-favorite-movies/static/js/2.6bc95c39.chunk.js"
+  },
+  {
+    "revision": "9b86730f961e04d356fb",
+    "url": "/my-favorite-movies/static/js/main.de2dae10.chunk.js"
+  },
+  {
+    "revision": "b9d0664f5e65262dded2",
+    "url": "/my-favorite-movies/static/js/runtime~main.e9fc2f1e.js"
+  }
+]);
